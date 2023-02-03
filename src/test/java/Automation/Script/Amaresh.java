228507f38ef2c4d87bcc31515cf861923386acf4
amaresh.nayak6@gmail.com <amaresh.nayak6@gmail.com>
2023-02-03 22:25:39 +0530

    amaresh

---
+++ src/test/java/Automation/Script/Amaresh.java
@@ -0,0 +1,12 @@
+package Automation.Script;
+
+public class Amaresh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("amaresh");
+
+	}
+
+}
